f3e09ec1bdb4e6706da284cb4f695b69476f6668
rithwik94 <rghanta2@illinois.edu>
2025-11-14 12:42:55 -0600

    percolation_data_for_polymerizaitonPeriods

--- percolate.py
+++ percolate.py
@@ -3,8 +3,11 @@ import signac
 
 # our signac project
 project = signac.get_project()
-for job in project: 
-    if job.sp["replica_index"]!=1:
-        continue
-    polymerize_file = "./workspace/" + str(job.id) + "/polymerize.gsd"
-    os.system("./percolation-analyzer/bin/sample_graph -i " + polymerize_file + " -o " + "./workspace/" + str(job.id) + "/percolation_data.txt")
+l = ["56fb642c37467c91ec211c993a54b0e0",
+"1c6da38a4491c2d8cd4c2768180d0aef",
+"665f7582c0248e3657ea1b0a25e47be2",
+"0214851522083aa91b4c02c4d2a55495"]
+
+for job in l:
+    polymerize_file = "./workspace/" + job+ "/polymerize.gsd"
+    os.system("./percolation-analyzer/bin/sample_graph -i " + polymerize_file + " -o " + "./" + job + "_percolation_data.txt")
