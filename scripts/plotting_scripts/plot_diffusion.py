b89c93b57ee7f060873fcceb24111a9017f69137
bcjeong1616 <bcjeong1616@gmail.com>
2025-11-24 15:36:41 -0600

    Moving scripts to plotting script folder

--- scripts/plotting_scripts/plot_diffusion.py
+++ scripts/plotting_scripts/plot_diffusion.py
@@ -142,12 +142,11 @@ END**
 '''
 
 for job in project: 
-    # if the job has been deformed
+    # if the job has been polymerized
     if job.isfile('polymerize.gsd'):
         with open(job.fn('signac_statepoint.json')) as f:
             statepoint = json.load(f)
 
-        # try:
         ''' initial look '''
         # get and trajectory and make sure the first frame checks out
         trajectory = gsd.hoomd.open(job.fn('polymerize.gsd'))
@@ -171,32 +170,7 @@ for job in project:
                 avg_diffusions.append(np.average(diffusions))
         print("frames",frames)
         print("avg_diff:",avg_diffusions)
-                    
-            
-        # ### jsons ------------------------------------------
-        # json_dir = "./workspace/" + str(job) + "/json/"
 
-        # if not os.path.isdir(json_dir):
-        #     os.mkdir(json_dir)
-        # # make a json for number of bonds on each atom through the trajectory
-        # with open(json_dir + 'bond_histograms.json', "w") as f :
-        #     json.dump(trajectory_bond_histograms, f)
-
-        # # make a json for number of radicals through the trajectory
-        # with open(json_dir + 'radical_numbers.json', "w") as f:
-        #     json.dump(trajectory_radical_numbers, f)
-
-        # # make a json for histogram of molecule sizes at last frame
-        # molec_size_hist = dict()
-        # for i in strand_lengths:
-        #     molec_size_hist[i] = molec_size_hist.get(i, 0) + 1
-        # with open(json_dir + 'molecules_histogram.json', "w") as f :
-        #     json.dump(trajectory_molecule_sizes, f)
-        
-        # # make a json for histogram of undreacted atoms at last frame
-        # with open(json_dir + 'non_reacted_histogram.json', "w") as f :
-        #     json.dump(trajectory_unreacted_atoms, f)
-        
         ### plots ------------------------------------------
         plot_dir = "./workspace/" + str(job) + "/plots/"
         if not os.path.isdir(plot_dir):
@@ -207,62 +181,9 @@ for job in project:
         ax.plot(frames,
                 avg_diffusions,
                 linewidth=2)
-                    # N = 5
-        # ax.plot(np.convolve(unique_strains,np.ones(N)/N,mode='valid'),
-        #         np.convolve(unique_true_stress_straight,np.ones(N)/N,mode='valid'),
-        #         linewidth=2,label=r'$\sigma_{xx}$')
-        # ax.plot(unique_strains,unique_true_stress_straight)
-        # ax[1].plot(strain,true_stress_deviatoric)
-        # plt.savefig(plot_dir + 'stress_strain.png')
 
         plt.savefig(plot_dir + 'stress_strain.png')
 
-        # c = rgb2hex(c)
-        # # molecule size histogram at final time point
-        # mol_size_fig = make_plotly_fig()
-        # mol_size_fig.update_layout(bargap=0)
-        # molec_size_hist = OrderedDict(sorted(molec_size_hist.items()))
-        # mol_size_fig.add_bar(name=i, x=[str(i) for i in list(molec_size_hist.keys())], y=list(molec_size_hist.values()),marker_color=c)
-        # mol_size_fig.write_image(plot_dir + "molecule_size_histogram.pdf")
-        
-        # # unreacted atoms histogram at final time point
-        # unreact_fig = make_plotly_fig()
-        # unreact_fig.update_layout(bargap=0)
-        # unreact_fig.add_bar(name=i,x=[str(i) for i in unreact_hist.keys()], y=list(unreact_hist.values()), marker_color=c)
-        # unreact_fig.write_image(plot_dir + "unreacted_atoms_histogram.pdf")
-
-        # # bond number histogram at final time point
-        # bond_no_fig = make_plotly_fig()
-        # bond_no_fig.update_layout(bargap=0)
-        # bond_no_fig.add_bar(name=i,x=list(bond_histogram.keys()), y=list(bond_histogram.values()), marker_color=c)
-        # bond_no_fig.write_image(plot_dir + "bond_number_histogram.pdf")
-
-        # # radical consistency
-        # radical_fig = make_plotly_fig(xaxis='Frames', yaxis='Number of radicals', yaxis_range=[7.75, 10.25])
-        # radical_fig.update_yaxes(tickvals=[8,9,10])
-        # radical_fig.add_hline(y=10, line_dash="dash", line_color="grey")
-        # radical_fig.add_trace(
-        #                       go.Scatter(x=list(trajectory_radical_numbers.keys()), 
-        #                                  y = [i[0] for i in trajectory_radical_numbers.values()],
-        #                                  mode='markers', 
-        #                                  marker=dict(color=c, opacity=0.5)
-        #                                  )
-        #                       )
-        # radical_fig.write_image(plot_dir + "radical_consistency.pdf")
-        
-        # # strands between crosslinks
-        # if group_plot_dir != None:
-        #     if not os.path.isdir(group_plot_dir + "moleculue_sizes/"):
-        #         os.mkdir(group_plot_dir + "moleculue_sizes/")
-        #         os.mkdir(group_plot_dir + "unreacted_atoms/")
-        #         os.mkdir(group_plot_dir + "bond_numbers/")
-        #         os.mkdir(group_plot_dir + "radical_consistency")
-        #     mol_size_fig.write_image(group_plot_dir + "moleculue_sizes/" + "{}.pdf".format(job.id))
-        #     unreact_fig.write_image(group_plot_dir + "unreacted_atoms/" + "{}.pdf".format(job.id))
-        #     bond_no_fig.write_image(group_plot_dir + "bond_numbers/" + "{}.pdf".format(job.id))
-        #     radical_fig.write_image(group_plot_dir + "radical_consistency/" + "{}.pdf".format(job.id))
-        # except:
-        #     print("file exist but 0 size", job.id)
 plt.show()
         
         
